42899bf4c18c177c820b833e61d66ff4782e4820
John Hawthorn <john@hawthorn.email>
2019-11-09 03:33:44 -0800

    Add fzy source files

---
+++ ext/fzy/match.h
@@ -0,0 +1,14 @@
+#ifndef MATCH_H
+#define MATCH_H MATCH_H
+
+#include <math.h>
+
+typedef double score_t;
+#define SCORE_MAX INFINITY
+#define SCORE_MIN -INFINITY
+
+int has_match(const char *needle, const char *haystack);
+score_t match_positions(const char *needle, const char *haystack, size_t *positions);
+score_t match(const char *needle, const char *haystack);
+
+#endif
