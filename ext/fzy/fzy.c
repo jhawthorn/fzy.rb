b242ca7611059f028669cd57bd109878433e3813
John Hawthorn <john@hawthorn.email>
2019-11-09 03:50:41 -0800

    Implement has_match

--- ext/fzy/fzy.c
+++ ext/fzy/fzy.c
@@ -1,9 +1,21 @@
 #include "fzy.h"
+#include "match.h"
 
-VALUE rb_mFzy;
+static VALUE rb_mFzy;
+
+static VALUE
+rb_has_match(VALUE _, VALUE needle, VALUE haystack) {
+  const char *needle_str = StringValueCStr(needle);
+  const char *haystack_str = StringValueCStr(haystack);
+  int match = has_match(needle_str, haystack_str);
+
+  return match ? Qtrue : Qfalse;
+}
 
 void
 Init_fzy(void)
 {
   rb_mFzy = rb_define_module("Fzy");
+
+  rb_define_module_function(rb_mFzy, "has_match", rb_has_match, 2);
 }
---
+++ test/match_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class MatchTest < Minitest::Test
+  def assert_has_match(needle, haystack)
+    msg = "Expected '#{needle}' to match '#{haystack}'"
+    assert Fzy.has_match(needle, haystack), msg
+  end
+
+  def refute_has_match(needle, haystack)
+    msg = "Expected '#{needle}' not to match '#{haystack}'"
+    refute Fzy.has_match(needle, haystack), msg
+  end
+
+  def test_exact_match_should_return_true
+    assert_has_match("a", "a")
+  end
+
+  def test_partial_match_should_return_true
+    assert_has_match("a", "ab")
+    assert_has_match("a", "ba")
+  end
+
+  def test_match_with_delimiters_in_between
+    assert_has_match("abc", "a|b|c")
+  end
+
+  def test_non_match_should_return_false
+    refute_has_match("a", "")
+    refute_has_match("a", "b")
+    refute_has_match("ass", "tags")
+  end
+
+  def test_empty_query_should_always_match
+    assert_has_match("", "")
+    assert_has_match("", "a")
+  end
+end
