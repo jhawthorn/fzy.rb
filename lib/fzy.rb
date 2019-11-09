95df6c90ce437393ba54206146c540970399d6a1
John Hawthorn <john@hawthorn.email>
2019-11-09 04:21:56 -0800

    Add Fzy::Match convenience method

--- lib/fzy.rb
+++ lib/fzy.rb
@@ -3,5 +3,25 @@ require "fzy/fzy"
 
 module Fzy
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Match
+    def initialize(needle, haystack)
+      @needle = needle
+      @haystack = haystack
+    end
+
+    def score
+      @score ||= Fzy.score(@needle, @haystack)
+    end
+
+    def positions
+      @positions ||= Fzy.positions(@needle, @haystack)
+    end
+  end
+
+  def self.match(needle, haystack)
+    if has_match?(needle, haystack)
+      Match.new(needle, haystack)
+    end
+  end
 end
