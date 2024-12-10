75d687bd8a8d320f3512639eadc49a6d48ea88e3
John Hawthorn <john@hawthorn.email>
2024-12-09 18:03:21 -0800

    Update code from fzy

--- ext/fzy/match.h
+++ ext/fzy/match.h
@@ -3,12 +3,22 @@
 
 #include <math.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef double score_t;
 #define SCORE_MAX INFINITY
 #define SCORE_MIN -INFINITY
 
+#define MATCH_MAX_LEN 1024
+
 int has_match(const char *needle, const char *haystack);
 score_t match_positions(const char *needle, const char *haystack, size_t *positions);
 score_t match(const char *needle, const char *haystack);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
